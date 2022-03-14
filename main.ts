db24aab2ec02806724a74c8b30249c68f9ed6259
zoe1111111 <100166925+zoe1111111@users.noreply.github.com>
2022-03-13 20:27:02 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+for (let index = 0; index <= 2; index++) {
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    basic.showNumber(3 - index)
+}
+music.playTone(392, music.beat(BeatFraction.Whole))
+basic.showString("Go!")
+basic.forever(function () {
+	
+})
